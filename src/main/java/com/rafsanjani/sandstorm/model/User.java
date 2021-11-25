67e0bebbf8af3ae1b8bf1075cd714b9616b78ae9
sadatrafsanjani <sadatrafsanjani@gmail.com>
2021-11-25 22:10:30 +0600

    system stabilized

--- src/main/java/com/rafsanjani/sandstorm/model/User.java
+++ src/main/java/com/rafsanjani/sandstorm/model/User.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private long id;
 
